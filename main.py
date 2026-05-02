25a61aeffd66501d2001aeaee2cb060058b1691e
Davikch <davidogluzd@gmail.com>
2026-05-02 20:41:36 +1000

    Update script to output movie review-tonality; add gitignored reviews folder

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 # https://ollama.com/blog/structured-outputs
 from ollama import chat
 from pydantic import BaseModel
+from typing import Literal
+from pathlib import Path
 
-class Country(BaseModel):
-  name: str
-  capital: str
-  languages: list[str]
+class ToneResponse(BaseModel):
+    tone: Literal['positive', 'negative']
+    confidence: float
 
-response = chat(
-  messages=[
-    {
-      'role': 'user',
-      'content': 'Tell me about Canada.',
-    }
-  ],
-  model='llama3',
-  format=Country.model_json_schema(),
-)
+for review in Path.cwd().glob('reviews/*'):
+    review_text = open(review).read()
+    response = chat(
+      messages=[
+        {
+          'role': 'user',
+          'content': f'''
+          {review_text}
 
-country = Country.model_validate_json(response.message.content)
-print(country)
+          Is this a negative or a positive movie review? Provide tone and confidence values.
+          ''',
+        }
+      ],
+      model='llama3',
+      options={'temperature': 0.3},
+      format=ToneResponse.model_json_schema(),
+    )
+
+    output = ToneResponse.model_validate_json(response.message.content)
+    print(review_text, output, sep='\n', end="\n\n")
+
